a6964cff53150bbbe910121063aac4dd182137bd
manojbaniya444 <86000102+manojbaniya444@users.noreply.github.com>
2024-01-09 00:40:26 +0545

    Lemon/Middleware support

--- HTTP/Lemon/Lemon.js
+++ HTTP/Lemon/Lemon.js
@@ -7,40 +7,67 @@ class Lemon {
     this.routes = {};
     this.middlewares = [];
 
-    // *Handling the requests
     this.server.on("request", async (request, response) => {
-    //   console.log("request received");
       response.sendFile = async function (path, mime) {
         const fileHandle = await fs.open(path, "r");
         const readStream = fileHandle.createReadStream();
         response.setHeader("Content-Type", mime);
         readStream.pipe(response);
       };
-      // *Handling unmatched routes
-      if (!this.routes[request.method.toLowerCase() + request.url]) {
-        response.statusCode = 404;
-        response.setHeader("Content-Type", "application/json");
-        return response.end(
-          JSON.stringify({ message: `Error: ${request.method} ${request.url}` })
-        );
-      }
 
-      // executing the request
-      this.routes[request.method.toLowerCase() + request.url](
-        request,
-        response
-      );
+      response.status = function (statusCode) {
+        response.statusCode = statusCode;
+        return;
+      };
+
+      response.json = function (data) {
+        response.setHeader("Content-Type", "Application/json");
+        response.write(JSON.stringify(data));
+        response.end();
+      };
+
+      //---------Running middlewares before response---------
+
+      const runMiddleware = (request, response, middlewares, index) => {
+        if (index === middlewares.length) {
+          //---------Handling unmatched routes---------
+          if (!this.routes[request.method.toLowerCase() + request.url]) {
+            response.statusCode = 404;
+            response.setHeader("Content-Type", "application/json");
+            return response.end(
+              JSON.stringify({
+                message: `Error: ${request.method} ${request.url}`,
+              })
+            );
+          }
+          //---------executing the request---------
+          this.routes[request.method.toLowerCase() + request.url](
+            request,
+            response
+          );
+        } else {
+          middlewares[index](request, response, () => {
+            runMiddleware(request, response, middlewares, index + 1);
+          });
+        }
+      };
+
+      runMiddleware(request, response, this.middlewares, 0);
     });
   }
 
-  // *Defining the routes
+  //---------Handling middlewares---------
+  before(cb) {
+    // lemon.before((req,res, next) => {})
+    this.middlewares.push(cb);
+  }
+
   route(method, path, cb) {
     // lemon.route("get", "/", (req, res) => {});
     // cb = (req,res) ={}
     this.routes[method + path] = cb;
   }
 
-  // *Starting the server
   listen(port, cb) {
     this.server.listen(port, () => {
       cb();
--- HTTP/Lemon/server.js
+++ HTTP/Lemon/server.js
@@ -1,11 +1,36 @@
 const Lemon = require("./Lemon");
 
-const lemon = new Lemon();
+const app = new Lemon();
 
-lemon.route("get", "/", (req, res) => {
-    res.sendFile("../web-server/public/index.html", "text/html");
+//---------Middlewares---------
+
+app.before((req, res, next) => {
+  if (Math.floor(Math.random() * 10) % 2 === 0) {
+    res.status(500);
+    res.json({ message: "Error: 500" });
+  } else {
+    next();
+  }
 });
 
-lemon.listen(9000, () => {
+//---------Routes---------
+
+app.route("get", "/", (req, res) => {
+  res.sendFile("../web-server/public/index.html", "text/html");
+});
+
+app.route("get", "/index.js", (req, res) => {
+  res.sendFile("../web-server/public/index.js", "text/javascript");
+});
+
+app.route("get", "/style.css", (req, res) => {
+  res.sendFile("../web-server/public/style.css", "text/css");
+});
+
+app.route("get", "/about", (req, res) => {
+  res.sendFile("../web-server/public/index.html", "text/html");
+});
+
+app.listen(9000, () => {
   console.log("Server running port: 9000");
 });
